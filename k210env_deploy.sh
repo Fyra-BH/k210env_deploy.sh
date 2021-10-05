511b0511868a22295ebf99264ac77e056191f5c8
Fyra-BH <58434746+Fyra-BH@users.noreply.github.com>
2021-10-05 22:03:59 +0800

    Create k210env_deploy.sh

---
+++ k210env_deploy.sh
@@ -0,0 +1,22 @@
+sudo apt update
+sudo apt upgrade
+sudo apt install make cmake wget -y
+cd ~
+mkdir -p kendryte/tarballs
+cd kendryte/tarballs
+wget https://hub.fastgit.org/kendryte/kendryte-gnu-toolchain/releases/download/v8.2.0-20190213/kendryte-toolchain-ubuntu-amd64-8.2.0-20190213.tar.gz
+wget https://hub.fastgit.org/kendryte/kendryte-standalone-sdk/archive/refs/tags/V0.5.6.tar.gz
+cd ..
+tar vxf tarballs/kendryte-toolchain-ubuntu-amd64-8.2.0-20190213.tar.gz
+tar vxf tarballs/V0.5.6.tar.gz
+cd ~/kendryte/kendryte-toolchain/bin/
+export PATH=$PATH:$(pwd)
+
+# 下面开始编译hello_world工程
+
+cd kendryte-standalone-sdk-0.5.6
+cd ~/kendryte/kendryte-standalone-sdk-0.5.6/
+mkdir -p build && cd build
+cmake .. -DPROJ=hello_world
+make -j8
+# 完成
