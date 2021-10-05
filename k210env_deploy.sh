e804195d798bd6058b3a659c1e6d94882cb1558d
Fyra-BH <bright_hu@foxmail.com>
2021-10-05 22:59:18 +0800

    压缩包后缀名修改

--- k210env_deploy.sh
+++ k210env_deploy.sh
@@ -7,7 +7,7 @@ cd kendryte/tarballs
 wget https://hub.fastgit.org/kendryte/kendryte-gnu-toolchain/releases/download/v8.2.0-20190409/kendryte-toolchain-ubuntu-amd64-8.2.0-20190409.tar.xz
 wget https://hub.fastgit.org/kendryte/kendryte-standalone-sdk/archive/refs/tags/V0.5.6.tar.gz
 cd ..
-tar vxf tarballs/kendryte-toolchain-ubuntu-amd64-8.2.0-20190409.tar.gz
+tar vxf tarballs/kendryte-toolchain-ubuntu-amd64-8.2.0-20190409.tar.xz
 tar vxf tarballs/V0.5.6.tar.gz
 cd ~/kendryte/kendryte-toolchain/bin/
 export PATH=$PATH:$(pwd)
