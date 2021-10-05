eb07dd91df70f88510945a3301cde8f8c40cf119
Fyra-BH <bright_hu@foxmail.com>
2021-10-05 22:54:19 +0800

    更换工具链版本

--- k210env_deploy.sh
+++ k210env_deploy.sh
@@ -1,10 +1,10 @@
 apt update
-apt upgrade
+apt upgrade -y
 apt install make cmake wget -y
 cd ~
 mkdir -p kendryte/tarballs
 cd kendryte/tarballs
-wget https://hub.fastgit.org/kendryte/kendryte-gnu-toolchain/releases/download/v8.2.0-20190213/kendryte-toolchain-ubuntu-amd64-8.2.0-20190213.tar.gz
+wget https://hub.fastgit.org/kendryte/kendryte-gnu-toolchain/releases/download/v8.2.0-20190409/kendryte-toolchain-ubuntu-amd64-8.2.0-20190409.tar.xz
 wget https://hub.fastgit.org/kendryte/kendryte-standalone-sdk/archive/refs/tags/V0.5.6.tar.gz
 cd ..
 tar vxf tarballs/kendryte-toolchain-ubuntu-amd64-8.2.0-20190213.tar.gz
