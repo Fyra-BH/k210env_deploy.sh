0d45524b723daa917884245427b95e63dd24b6a5
Fyra-BH <58434746+Fyra-BH@users.noreply.github.com>
2022-09-16 11:48:27 +0800

    Update k210env_deploy.sh
    fastorg域名变动

--- k210env_deploy.sh
+++ k210env_deploy.sh
@@ -4,8 +4,8 @@ apt install make cmake wget -y
 cd ~
 mkdir -p kendryte/tarballs
 cd kendryte/tarballs
-wget https://hub.fastgit.org/kendryte/kendryte-gnu-toolchain/releases/download/v8.2.0-20190409/kendryte-toolchain-ubuntu-amd64-8.2.0-20190409.tar.xz
-wget https://hub.fastgit.org/kendryte/kendryte-standalone-sdk/archive/refs/tags/V0.5.6.tar.gz
+wget https://hub.fastgit.xyz/kendryte/kendryte-gnu-toolchain/releases/download/v8.2.0-20190409/kendryte-toolchain-ubuntu-amd64-8.2.0-20190409.tar.xz
+wget https://hub.fastgit.xyz/kendryte/kendryte-standalone-sdk/archive/refs/tags/V0.5.6.tar.gz
 cd ..
 tar vxf tarballs/kendryte-toolchain-ubuntu-amd64-8.2.0-20190409.tar.xz
 tar vxf tarballs/V0.5.6.tar.gz
